e8e03620aa1deb01f6f0e5dc7655e0758715d9ac
Azza <huangzhengcaiaar@hotmail.com>
2022-01-13 22:44:15 +0800

    change

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 export default function App() {
   const [loading, setLoading] = useState(true)
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139"
+
+function loadBusStopData() {
+  fetch(BUSSTOP_URL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((responseData) => {
+    console.log(responseData);
+  });
+  
+
+  useEffect(() => {
+    loadBusStopData();
+  }, []);
+}
+  
 
   return (
     <View style={styles.container}>
-      <Text>Bus arrival time: </Text>
-      <Text>{loading ? 'loading' : 'loaded'}</Text>
+      <Text style={styles.title}>Bus arrival time: </Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? <ActivityIndicator color="blue" size="large" /> : "loaded"}</Text>
       <TouchableOpacity style={styles.button}>
-        <Text>Refresh</Text>
+        <Text style={styles.buttonText}>Refresh!</Text>
       </TouchableOpacity>
     </View>
   );
-}
+  }
 
 const styles = StyleSheet.create({
   container: {
@@ -26,5 +44,5 @@ const styles = StyleSheet.create({
     backgroundColor: "green",
     padding: 10,
     margin: 10, 
-  }
+  },
 });
